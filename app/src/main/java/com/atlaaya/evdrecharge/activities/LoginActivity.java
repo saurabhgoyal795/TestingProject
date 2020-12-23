a8e85ccbc86e0d2fd794e27cc54ee28211a8dbb7
yogeshhelpful <007saurabhgoyal@gmail.com>
2020-12-23 14:56:46 +0530

    changes

--- app/src/main/java/com/atlaaya/evdrecharge/activities/LoginActivity.java
+++ app/src/main/java/com/atlaaya/evdrecharge/activities/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.databinding.DataBindingUtil;
 
@@ -34,13 +36,16 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
 
         presenter = new LoginPresenter();
         presenter.setView(this);
 
         String email = SessionManager.getString(this, SessionManager.KEY_EMAIL);
-        binding.textInputUserId.getEditText().setText(email);
+        binding.textInputUserId.setText(email);
 
         binding.btnLogin.setOnClickListener(this);
 
@@ -62,11 +67,9 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
     }
 
     private void onClickLogin() {
-        if (Objects.requireNonNull(binding.textInputUserId.getEditText()).getText().toString().trim().isEmpty()) {
-            binding.textInputUserId.requestFocus();
+        if (Objects.requireNonNull(binding.textInputUserId).getText().toString().trim().isEmpty()) {
             MyCustomToast.showErrorAlert(this, getString(R.string.alert_enter_user_id));
-        } else if (Objects.requireNonNull(binding.textInputPassword.getEditText()).getText().toString().trim().isEmpty()) {
-            binding.textInputPassword.requestFocus();
+        } else if (Objects.requireNonNull(binding.textInputPassword).getText().toString().trim().isEmpty()) {
             MyCustomToast.showErrorAlert(this, getString(R.string.alert_enter_password));
         } else {
             callLoginAPI();
@@ -77,8 +80,8 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
         if (CheckInternetConnection.isInternetConnection(this)) {
             HashMap<String, RequestBody> map = new HashMap<>();
             map.put("token", RequestBody.create(MediaType.parse("multipart/form-data"), AppConstants.App_TOKEN));
-            map.put("email", RequestBody.create(MediaType.parse("multipart/form-data"), binding.textInputUserId.getEditText().getText().toString().trim()));
-            map.put("password", RequestBody.create(MediaType.parse("multipart/form-data"), binding.textInputPassword.getEditText().getText().toString()));
+            map.put("email", RequestBody.create(MediaType.parse("multipart/form-data"), binding.textInputUserId.getText().toString().trim()));
+            map.put("password", RequestBody.create(MediaType.parse("multipart/form-data"), binding.textInputPassword.getText().toString()));
 
             presenter.login(this, map);
         } else {
@@ -90,8 +93,8 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
     public void onSuccess(ResponseLogin body) {
         if (body.isRESPONSE()) {
             SessionManager.saveLoginDetail(this,
-                    binding.textInputUserId.getEditText().getText().toString().trim(),
-                    binding.textInputPassword.getEditText().getText().toString());
+                    binding.textInputUserId.getText().toString().trim(),
+                    binding.textInputPassword.getText().toString());
             SessionManager.saveUserDetail(this, body.getUserInfo());
 
             startActivity(new Intent(this, HomeActivity.class));
--- app/src/main/java/com/atlaaya/evdrecharge/activities/SplashActivity.java
+++ app/src/main/java/com/atlaaya/evdrecharge/activities/SplashActivity.java
@@ -3,6 +3,8 @@ package com.atlaaya.evdrecharge.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +18,9 @@ public class SplashActivity extends AppCompatActivity {
 //        requestWindowFeature(Window.FEATURE_NO_TITLE);
 //        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
